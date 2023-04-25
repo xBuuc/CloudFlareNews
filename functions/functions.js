183ce4bb3ba6836d053cbbe711627862edbb00bb
xBuuc <piotrekkozak39@gmail.com>
2023-04-25 20:06:15 +0200

    update

--- functions/functions.js
+++ functions/functions.js
@@ -3,13 +3,13 @@ const pageSize = 10
 
   export async function onRequestGet({env, request}) {
 
-    const apiKey = env.apiKey.replace(' ', '');
+    const key = env.apiKey.replace(' ', '');
 
     const params = (new URL(request.url)).searchParams;
 
     const page = params.get('page');
 
-    const url = `https://newsapi.org/v2/top-headlines?country=us&pageSize=${pageSize}&page=${page}&apiKey=${apiKey}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=us&pageSize=${pageSize}&page=${page}&apiKey=${key}`;
     const res = await fetch(url);
     const articles = await res.json();
 
