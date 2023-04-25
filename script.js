3645c5ead61b0091c0564f1a6f30ad03efa02f24
xBuuc <piotrekkozak39@gmail.com>
2023-04-25 21:47:31 +0200

    update?

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ for (let i = 1; i <= 5; i++) {
       console.log(page);
     });
   }
-
+  
   const parser = new DOMParser();
   const buttons = document.getElementsByClassName('button');
   
@@ -14,25 +14,42 @@ for (let i = 1; i <= 5; i++) {
       const page = button.innerHTML;
       console.log(page);
   
-      const res = await fetch(`/functions?page=${page}`);
-      const body = await res.json();
-      console.log(body);
+      try {
+        const res = await fetch(`/functions?page=${page}`);
+        const body = await res.json();
+        console.log(body);
   
+        const articles = body.articles;
   
-      const title = body.articles.title;
-      const author = body.articles.author;
-      const url = body.articles.url;
-      const imageUrl = body.articles.urlToImage;
+        const articleList = parser.parseFromString('<ul></ul>', "text/html").body.firstChild;
   
-      const el = parser.parseFromString(`
-        <dl>
-          <dt>${title}</dt>
-          <dd>${author}</dd>
-          <dt>${url}</dt>
-          <dd>${imageUrl}</dd>
-        </dl>
-      `, "text/html").body.firstChild;
+        articles.forEach(article => {
+          const title = article.title;
+          const author = article.author;
+          const url = article.url;
+          const imageUrl = article.urlToImage;
   
-      button.after(el);
+          const articleItem = parser.parseFromString(`
+            <li>
+              <dl>
+                <dt>${title}</dt>
+                <dd>${author}</dd>
+                <dt>${url}</dt>
+                <dd>${imageUrl}</dd>
+              </dl>
+            </li>
+          `, "text/html").body.firstChild;
+  
+          articleList.appendChild(articleItem);
+        });
+  
+        const existingArticleList = document.getElementById('article-list');
+        existingArticleList.replaceWith(articleList);
+        articleList.id = 'article-list';
+  
+      } catch (error) {
+        console.error(error);
+      }
     });
   });
+  
